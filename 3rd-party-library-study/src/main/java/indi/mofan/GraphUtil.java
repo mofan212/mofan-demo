d3cb9ec47f861d2b5a09192cb7504f885add3fbf
mofan <cy.mofan@foxmail.com>
2022-10-09 15:46:35 +0800

    feat: 根据指定的开始节点和结束节点，获取两者之间的所有路径

---
+++ 3rd-party-library-study/src/main/java/indi/mofan/GraphUtil.java
@@ -0,0 +1,82 @@
+package indi.mofan;
+
+import com.google.common.graph.Graph;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author mofan
+ * @date 2022/10/9 13:50
+ */
+public class GraphUtil {
+
+    /**
+     * 获取两个节点之间的所有路径
+     *
+     * @param graph     图
+     * @param startNode 起始节点
+     * @param endNode   结束节点
+     * @param <T>       节点类型
+     * @return 所有路径信息（每条路径都是从开始节点到结束节点）
+     */
+    public static <T> List<List<T>> getAllPath(Graph<T> graph, T startNode, T endNode) {
+        Deque<T> main = new ArrayDeque<>();
+        Deque<Set<T>> secondary = new ArrayDeque<>();
+
+        // 所有路径
+        List<List<T>> path = new ArrayList<>();
+
+        main.push(startNode);
+        // 获取开始节点的所有后继节点并将塞入辅栈中（new 一个 Set，防止 UnsupportedOperationException）
+        secondary.push(new HashSet<>(graph.successors(startNode)));
+
+        while (CollectionUtils.isNotEmpty(main)) {
+            Set<T> secondaryPeek = secondary.peek();
+            if (CollectionUtils.isNotEmpty(secondaryPeek)) {
+                Optional<T> optional = secondaryPeek.stream().findAny();
+                if (optional.isPresent()) {
+                    T t = optional.get();
+                    main.push(t);
+                    secondaryPeek.remove(t);
+
+                    Set<T> successors = graph.successors(t);
+                    Set<T> collect = successors.stream().filter(i -> !main.contains(i)).collect(Collectors.toSet());
+                    secondary.push(collect);
+                }
+            } else { // 削栈
+                cutTwoStack(main, secondary);
+            }
+
+            if (Objects.equals(main.peek(), endNode)) {
+                List<T> onePath = new ArrayList<>();
+                // ArrayDeque 的 push() 是将元素放到第一个，因此需要倒着迭代一下
+                main.descendingIterator().forEachRemaining(onePath::add);
+                path.add(onePath);
+                // 再次削栈
+                cutTwoStack(main, secondary);
+            }
+        }
+        return path;
+    }
+
+    /**
+     * 削栈（移除两个栈的顶层节点）
+     *
+     * @param mainStack      主栈
+     * @param secondaryStack 辅栈
+     * @param <T>            节点类型
+     */
+    private static <T> void cutTwoStack(Deque<T> mainStack, Deque<Set<T>> secondaryStack) {
+        mainStack.pop();
+        secondaryStack.pop();
+    }
+}
--- 3rd-party-library-study/src/test/java/indi/mofan/GraphTest.java
+++ 3rd-party-library-study/src/test/java/indi/mofan/GraphTest.java
@@ -9,8 +9,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author mofan
@@ -36,5 +41,86 @@ public class GraphTest {
         Assert.assertTrue(integers.containsAll(new HashSet<>(Arrays.asList(1, 2, 4))));
     }
 
+    @Test
+    public void testGetAllPath() {
+        MutableGraph<Integer> graph = GraphBuilder.undirected()
+                .nodeOrder(ElementOrder.<Integer>insertion())
+                .expectedNodeCount(8)
+                .allowsSelfLoops(false)
+                .build();
 
+        graph.putEdge(3, 7);
+        graph.putEdge(3, 1);
+        graph.putEdge(1, 0);
+        graph.putEdge(1, 4);
+        graph.putEdge(7, 4);
+        graph.putEdge(4, 5);
+        graph.putEdge(0, 2);
+        graph.putEdge(2, 5);
+        graph.putEdge(2, 6);
+        graph.putEdge(5, 6);
+        graph.putEdge(6, 8);
+
+        List<List<Integer>> allPath = GraphUtil.getAllPath(graph, 3, 6);
+        for (List<Integer> path : allPath) {
+            System.out.println(path);
+        }
+    }
+    
+    @Test
+    public void testGetCyclePath() {
+        MutableGraph<Integer> graph = GraphBuilder.directed()
+                .nodeOrder(ElementOrder.<Integer>insertion())
+                .expectedNodeCount(3)
+                .allowsSelfLoops(false)
+                .build();
+
+        graph.putEdge(1, 2);
+        graph.putEdge(2, 3);
+        graph.putEdge(3, 1);
+        graph.putEdge(4, 1);
+
+        final int target = 1;
+
+        // 获取谁调用了 1
+        Graph<Integer> transpose = Graphs.transpose(graph);
+        Set<Integer> transposeReachableNodes = Graphs.reachableNodes(transpose, target);
+
+        // 调用了 1 的，并且入度为 0：底层节点
+        Set<Integer> sourceSet = transposeReachableNodes.stream()
+                .filter(i -> graph.inDegree(i) == 0)
+                .collect(Collectors.toSet());
+
+        // 获取顶层节点到目标节点的路径
+        List<List<Integer>> path = sourceSet.stream()
+                .map(i -> GraphUtil.getAllPath(graph, i, target))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        // 调用关系中是否存在间接自己调用自己的情况
+        boolean hasCycle = transposeReachableNodes.contains(target);
+
+        if (hasCycle) {
+            Set<Integer> reachableNodes = Graphs.reachableNodes(graph, target);
+
+            // 获取组成环的节点（不包含目标节点）
+            Set<Integer> cycleNodes = transposeReachableNodes.stream()
+                    .filter(reachableNodes::contains)
+                    .filter(i -> !Objects.equals(i, target))
+                    .collect(Collectors.toSet());
+
+            // 获取环节点到目标节点的最长路径
+            cycleNodes.stream()
+                    .map(i -> GraphUtil.getAllPath(graph, i, target))
+                    .flatMap(Collection::stream)
+                    .max(Comparator.comparingInt(List::size))
+                    .ifPresent(i -> {
+                        i.add(0, target);
+                        path.add(i);
+                    });
+
+            path.forEach(System.out::println);
+
+        }
+    }
 }
