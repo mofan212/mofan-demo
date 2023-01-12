ede3a0aab58c7680e46fe6ef451af799fa7f4ef1
mofan <cy.mofan@foxmail.com>
2023-01-12 21:30:58 +0800

    列表的处理与空指针判断

--- api-study/src/test/java/indi/mofan/ReflectionTest.java
+++ api-study/src/test/java/indi/mofan/ReflectionTest.java
@@ -6,10 +6,13 @@ import indi.mofan.dto.One2OneDto;
 import indi.mofan.dto.SubDto;
 import indi.mofan.util.ReflectionUtil;
 import lombok.SneakyThrows;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author mofan
@@ -22,30 +25,47 @@ public class ReflectionTest {
         GrandchildDto grandchildDto = new GrandchildDto();
         grandchildDto.setId(123456789L);
 
+        GrandchildDto secondGrandchildDto = new GrandchildDto();
+        secondGrandchildDto.setId(147258963L);
+
+        GrandchildDto thirdGrandchildDto = new GrandchildDto();
+        thirdGrandchildDto.setId(963852741L);
+
         SubDto subDto = new SubDto();
-        subDto.setGrandchildDtoList(new ArrayList<>(Collections.singletonList(grandchildDto)));
+        subDto.setGrandchildDtoList(new ArrayList<>(Arrays.asList(grandchildDto, secondGrandchildDto)));
+
+        SubDto anotherSubDto = new SubDto();
+        anotherSubDto.setGrandchildDtoList(new ArrayList<>(Collections.singletonList(thirdGrandchildDto)));
 
         One2OneDto one2OneDto = new One2OneDto();
         one2OneDto.setNumber(10);
+
+        One2OneDto anotherOne2OneDto = new One2OneDto();
+        anotherOne2OneDto.setNumber(20);
 
         MainDto mainDto = new MainDto();
         mainDto.setSubDtoList(new ArrayList<>(Collections.singletonList(subDto)));
         mainDto.setStr("main");
         mainDto.setOne2OneDto(one2OneDto);
 
-        ReflectionUtil.doWith(Collections.singletonList(mainDto), new String[]{"subDtoList", "grandchildDtoList", "id"}, (field, object) -> {
-            field.set(object, 987654321L);
-        });
-        System.out.println(mainDto);
+        MainDto anotherMainDto = new MainDto();
+        anotherMainDto.setSubDtoList(new ArrayList<>(Collections.singletonList(anotherSubDto)));
+        anotherMainDto.setStr("MAIN");
+        anotherMainDto.setOne2OneDto(anotherOne2OneDto);
 
-        ReflectionUtil.doWith(Collections.singletonList(mainDto), new String[]{"str"}, (field, object) -> {
-            field.set(object, "test");
-        });
-        System.out.println(mainDto);
+        List<MainDto> mainDtoList = Arrays.asList(mainDto, anotherMainDto);
 
-        ReflectionUtil.doWith(Collections.singletonList(mainDto), new String[]{"one2OneDto", "number"}, (field, object) -> {
-            field.set(object, 123);
-        });
-        System.out.println(mainDto);
+        ReflectionUtil.doWith(mainDtoList, new String[]{"subDtoList", "grandchildDtoList", "id"}, (field, object) ->
+                field.set(object, 987654321L)
+        );
+        mainDtoList.forEach(i -> i.getSubDtoList().forEach(j -> j.getGrandchildDtoList().forEach(k ->
+                Assert.assertEquals(987654321L, k.getId().longValue())
+        )));
+
+        ReflectionUtil.doWith(mainDtoList, new String[]{"str"}, (field, object) -> field.set(object, "test"));
+        mainDtoList.forEach(i -> Assert.assertEquals("test", i.getStr()));
+
+        ReflectionUtil.doWith(mainDtoList, new String[]{"one2OneDto", "number"}, (field, object) -> field.set(object, 123));
+        mainDtoList.forEach(i -> Assert.assertEquals(123, i.getOne2OneDto().getNumber()));
     }
 }
