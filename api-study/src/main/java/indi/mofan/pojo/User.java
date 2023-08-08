143972f90789aa42cf8b61b33adfca5b5351a854
mofan <cy.mofan@foxmail.com>
2023-08-08 21:56:28 +0800

    正确使用 Lombok

--- api-study/src/main/java/indi/mofan/pojo/User.java
+++ api-study/src/main/java/indi/mofan/pojo/User.java
@@ -1,15 +1,8 @@
 package indi.mofan.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
 /**
  * @author mofan
  * @date 2022/12/16 10:33
  */
-@Getter
-@AllArgsConstructor
-@ToString
 public record User(String name, int age) {
 }
