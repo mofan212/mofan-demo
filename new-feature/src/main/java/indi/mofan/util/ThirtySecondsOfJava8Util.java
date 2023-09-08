e6563f691343245c0a8e5ff557fb53caeb74773d
mofan <cy.mofan@foxmail.com>
2023-09-08 21:39:56 +0800

    int 数组相关 stream 训练

--- new-feature/src/main/java/indi/mofan/util/ThirtySecondsOfJava8Util.java
+++ new-feature/src/main/java/indi/mofan/util/ThirtySecondsOfJava8Util.java
@@ -6,34 +6,120 @@ import org.apache.commons.lang3.ArrayUtils;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author mofan
  * @date 2023/9/7 20:21
  */
 public class ThirtySecondsOfJava8Util {
-    /**
-     * 按给定 size 进行分块
-     */
-    public static <T> List<List<T>> chunkWithObj(List<T> nums, int size) {
-        if (CollectionUtils.isEmpty(nums)) {
-            return Collections.emptyList();
+
+    public final static class Array {
+        /**
+         * 按给定 size 进行分块
+         */
+        public static <T> List<List<T>> chunkWithObj(List<T> nums, int size) {
+            if (CollectionUtils.isEmpty(nums)) {
+                return Collections.emptyList();
+            }
+            return IntStream.iterate(0, i -> i + size)
+                    // ceil 向上取整，确定有多少组
+                    .limit((long) Math.ceil((double) nums.size() / size))
+                    .mapToObj(i -> nums.subList(i, Math.min(i + size, nums.size())))
+                    .toList();
         }
-        return IntStream.iterate(0, i -> i + size)
-                // ceil 向上取整，确定有多少组
-                .limit((long) Math.ceil((double) nums.size() / size))
-                .mapToObj(i -> nums.subList(i, Math.min(i + size, nums.size())))
-                .toList();
+
+        public static int[][] chunkWithInt(int[] nums, int size) {
+            if (ArrayUtils.isEmpty(nums)) {
+                return new int[0][0];
+            }
+            return IntStream.iterate(0, i -> i + size)
+                    .limit((long) Math.ceil((double) nums.length / size))
+                    .mapToObj(i -> Arrays.copyOfRange(nums, i, Math.min(i + size, nums.length)))
+                    .toArray(int[][]::new);
+        }
+
+        @SuppressWarnings("unchecked")
+        public static <T> T[] concat(T[] first, T[] second) {
+            return Stream.concat(Stream.of(first), Stream.of(second))
+                    .toArray(i -> (T[]) Arrays.copyOf(new Object[0], i, first.getClass()));
+        }
+
+        public static long countOccurrences(int[] numbers, int value) {
+            return Arrays.stream(numbers)
+                    .filter(i -> i == value)
+                    .count();
+        }
+
+        /**
+         * 从 first 中找出不包含 second 的元素组成的数组
+         */
+        public static int[] difference(int[] first, int[] second) {
+            Set<Integer> secondSet = Arrays.stream(second).boxed().collect(Collectors.toSet());
+            return Arrays.stream(first).filter(i -> !secondSet.contains(i)).toArray();
+        }
+
+        /**
+         * comparator == 0 表示相等
+         */
+        public static int[] differenceWith(int[] first, int[] second, IntBinaryOperator comparator) {
+            return Arrays.stream(first)
+                    .filter(i -> Arrays.stream(second).noneMatch(j -> comparator.applyAsInt(i, j) == 0))
+                    .toArray();
+        }
+
+        /**
+         * 去重
+         */
+        public static int[] distinctValuesOfArray(int[] elements) {
+            return Arrays.stream(elements).distinct().toArray();
+        }
+
+        /**
+         * 删除元素，直到 condition == true
+         */
+        public static int[] dropElements(int[] elements, IntPredicate condition) {
+            return IntStream.range(0, elements.length)
+                    .filter(i -> condition.test(elements[i]))
+                    .findFirst()
+                    .stream()
+                    .mapToObj(i -> Arrays.copyOfRange(elements, i, elements.length))
+                    .findFirst()
+                    .orElse(new int[0]);
+        }
+
+        /**
+         * 从右边移除 n 个元素
+         */
+        public static int[] dropRight(int[] elements, int n) {
+            if (n < 0) {
+                throw new IllegalArgumentException("n is less than 0");
+            }
+            return n < elements.length ? Arrays.copyOfRange(elements, 0, elements.length - n) : new int[0];
+        }
+
+        /**
+         * 每个第 n 个元素
+         */
+        public static int[] everyNth(int[] elements, int nth) {
+            return IntStream.range(0, elements.length)
+                    .filter(i -> i % nth == nth - 1)
+                    .map(i -> elements[i])
+                    .toArray();
+        }
+
+        public static int indexOf(int[] elements, int el) {
+            return IntStream.range(0, elements.length)
+                    .filter(i -> elements[i] == el)
+                    .findFirst()
+                    .orElse(-1);
+        }
     }
 
-    public static int[][] chunkWithInt(int[] nums, int size) {
-        if (ArrayUtils.isEmpty(nums)) {
-            return new int[0][0];
-        }
-        return IntStream.iterate(0, i -> i + size)
-                .limit((long) Math.ceil((double) nums.length / size))
-                .mapToObj(i -> Arrays.copyOfRange(nums, i, Math.min(i + size, nums.length)))
-                .toArray(int[][]::new);
-    }
+
 }
--- new-feature/src/test/java/indi/mofan/stream/ThirtySecondsOfJava8Test.java
+++ new-feature/src/test/java/indi/mofan/stream/ThirtySecondsOfJava8Test.java
@@ -16,7 +16,7 @@ public class ThirtySecondsOfJava8Test implements WithAssertions {
     @Test
     public void testChunk() {
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-        List<List<Integer>> chunk = ThirtySecondsOfJava8Util.chunkWithObj(list, 3);
+        List<List<Integer>> chunk = ThirtySecondsOfJava8Util.Array.chunkWithObj(list, 3);
         assertThat(chunk).hasSize(4)
                 .containsExactly(
                         List.of(1, 2, 3),
@@ -26,10 +26,93 @@ public class ThirtySecondsOfJava8Test implements WithAssertions {
                 );
 
         int[] ints = list.stream().mapToInt(Integer::intValue).toArray();
-        int[][] result = ThirtySecondsOfJava8Util.chunkWithInt(ints, 3);
+        int[][] result = ThirtySecondsOfJava8Util.Array.chunkWithInt(ints, 3);
         assertThat(result).contains(new int[]{1, 2, 3}, Index.atIndex(0))
                 .contains(new int[]{4, 5, 6}, Index.atIndex(1))
                 .contains(new int[]{7, 8, 9}, Index.atIndex(2))
                 .contains(new int[]{0}, Index.atIndex(3));
+    }
+
+    @Test
+    public void testConcat() {
+        Integer[] ints1 = {1, 2, 3};
+        Integer[] ints2 = {4, 5, 6};
+        Integer[] result = ThirtySecondsOfJava8Util.Array.concat(ints1, ints2);
+        assertThat(result).hasSize(6).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    public void testCountOccurrences() {
+        int[] ints = {1, 2, 2, 3, 3, 3};
+        long result = ThirtySecondsOfJava8Util.Array.countOccurrences(ints, 2);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    public void testDifference() {
+        int[] ints1 = {1, 2, 3, 4};
+        int[] ints2 = {2, 2, 3};
+        int[] difference = ThirtySecondsOfJava8Util.Array.difference(ints1, ints2);
+        assertThat(difference).hasSize(2).containsExactly(1, 4);
+    }
+
+    @Test
+    public void testDifferenceWith() {
+        int[] ints1 = {1, 2, 3, 4};
+        int[] ints2 = {2, 2, 3, 3, 3, 4, 4, 4, 4};
+        int[] result = ThirtySecondsOfJava8Util.Array.differenceWith(ints1, ints2, Math::subtractExact);
+        assertThat(result).hasSize(1).containsExactly(1);
+    }
+
+    @Test
+    public void testDistinctValuesOfArray() {
+        int[] ints = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+        int[] result = ThirtySecondsOfJava8Util.Array.distinctValuesOfArray(ints);
+        assertThat(result).hasSize(4).containsExactly(1, 2, 3, 4);
+    }
+
+    @Test
+    public void testDropElements() {
+        int[] ints = {1, 2, 2, 3, 3, 3};
+        int[] result = ThirtySecondsOfJava8Util.Array.dropElements(ints, value -> value == 3);
+        assertThat(result).hasSize(3).containsOnly(3);
+
+        result = ThirtySecondsOfJava8Util.Array.dropElements(ints, value -> value == 4);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void testDropRight() {
+        int[] ints = {1, 2, 3, 4, 5, 6};
+        int[] result = ThirtySecondsOfJava8Util.Array.dropRight(ints, 3);
+        assertThat(result).hasSize(3).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    public void testEveryNth() {
+        int[] ints = {0, 1, 2, 3, 4, 5};
+        int[] result = ThirtySecondsOfJava8Util.Array.everyNth(ints, 1);
+        assertThat(result).hasSize(ints.length);
+
+        result = ThirtySecondsOfJava8Util.Array.everyNth(ints, 2);
+        assertThat(result).hasSize(3).containsExactly(1, 3, 5);
+
+        result = ThirtySecondsOfJava8Util.Array.everyNth(ints, 3);
+        assertThat(result).hasSize(2).containsExactly(2, 5);
+
+        result = ThirtySecondsOfJava8Util.Array.everyNth(ints, 4);
+        assertThat(result).hasSize(1).containsExactly(3);
+
+        result = ThirtySecondsOfJava8Util.Array.everyNth(ints, 7);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void testIndexOf() {
+        int[] ints = {1, 2, 3};
+        int index = ThirtySecondsOfJava8Util.Array.indexOf(ints, 2);
+        assertThat(index).isEqualTo(1);
+        index = ThirtySecondsOfJava8Util.Array.indexOf(ints, 0);
+        assertThat(index).isEqualTo(-1);
     }
 }
