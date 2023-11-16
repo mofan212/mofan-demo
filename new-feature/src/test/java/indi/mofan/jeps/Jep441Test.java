fb7814ff343d1026d1aafffb175a94cedadf2e33
mofan <cy.mofan@foxmail.com>
2023-11-16 23:14:09 +0800

    JEP 441: Pattern Matching for switch

--- new-feature/src/test/java/indi/mofan/jeps/Jep441Test.java
+++ new-feature/src/test/java/indi/mofan/jeps/Jep441Test.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 /**
  * @author mofan
@@ -76,5 +78,159 @@ public class Jep441Test implements WithAssertions {
     public enum Suit implements CardClassification {CLUBS, DIAMONDS, HEARTS, SPADES}
 
     static final class Tarot implements CardClassification {
+    }
+
+    private String exhaustiveSwitchWithBetterEnumSupport(CardClassification c) {
+        return switch (c) {
+            case Suit.CLUBS -> "C";
+            case Suit.DIAMONDS -> "D";
+            case Suit.HEARTS -> "H";
+            case Suit.SPADES -> "S";
+            case Tarot t -> "Tarot";
+        };
+    }
+
+    @Test
+    public void testSwitchesAndEnumConstants() {
+        assertThat(exhaustiveSwitchWithBetterEnumSupport(new Tarot())).isEqualTo("Tarot");
+        assertThat(exhaustiveSwitchWithBetterEnumSupport(Suit.CLUBS)).isEqualTo("C");
+        assertThat(exhaustiveSwitchWithBetterEnumSupport(Suit.DIAMONDS)).isEqualTo("D");
+        assertThat(exhaustiveSwitchWithBetterEnumSupport(Suit.HEARTS)).isEqualTo("H");
+        assertThat(exhaustiveSwitchWithBetterEnumSupport(Suit.SPADES)).isEqualTo("S");
+    }
+
+    sealed interface Currency permits Coin {
+    }
+
+    enum Coin implements Currency {HEADS, TAILS}
+
+    private String improveEnumConstant1(Currency c) {
+        // 传入的不是枚举，而是父接口，需要指明具体的枚举
+        return switch (c) {
+            case Coin.HEADS -> "H";
+            case Coin.TAILS -> "T";
+        };
+    }
+
+    private String improveEnumConstant2(Coin c) {
+        // 如果传入的是枚举，可以直接使用枚举项
+        return switch (c) {
+            case HEADS -> "H";
+            case TAILS -> "T";
+        };
+    }
+
+    @Test
+    public void testImproveEnumConstant() {
+        assertThat(improveEnumConstant1(Coin.HEADS)).isEqualTo("H");
+        assertThat(improveEnumConstant1(Coin.TAILS)).isEqualTo("T");
+
+        assertThat(improveEnumConstant2(Coin.HEADS)).isEqualTo("H");
+        assertThat(improveEnumConstant2(Coin.TAILS)).isEqualTo("T");
+    }
+
+    private String patternsInSwitchLabels(Object obj) {
+        return switch (obj) {
+            // 先匹配长度为 1 的字符串
+            case String s when s.length() == 1 -> "ONE";
+            // 再匹配其他长度的字符串
+            case String s -> "OTHER";
+            // 默认匹配方式
+            default -> "DEFAULT";
+        };
+    }
+
+    @Test
+    public void testPatternsInSwitchLabels() {
+        assertThat(patternsInSwitchLabels("1")).isEqualTo("ONE");
+        assertThat(patternsInSwitchLabels("test")).isEqualTo("OTHER");
+        assertThat(patternsInSwitchLabels(1)).isEqualTo("DEFAULT");
+    }
+
+    private String dominanceOfCaseLabels1(Object obj) {
+        // 和 try-catch 类似，前面 case 的范围不能包含后面
+        return switch (obj) {
+            case String s -> "String";
+            case CharSequence c -> "Chars";
+            default -> "Other";
+        };
+    }
+
+    private String dominanceOfCaseLabels2(Integer integer) {
+        return switch (integer) {
+            case -1, 1 -> "First";
+            // 大于 0，但又不是 -1 和 1
+            case Integer i when i > 0 -> "Positive";
+            case Integer i -> "Not Positive";
+        };
+    }
+
+    @Test
+    public void testDominanceOfCaseLabels() {
+        assertThat(dominanceOfCaseLabels1("test")).isEqualTo("String");
+        assertThat(dominanceOfCaseLabels1(new StringBuilder())).isEqualTo("Chars");
+        assertThat(dominanceOfCaseLabels1(1)).isEqualTo("Other");
+
+        for (Integer i : List.of(1, -1)) {
+            assertThat(dominanceOfCaseLabels2(i)).isEqualTo("First");
+        }
+
+        IntStream.generate(() -> ThreadLocalRandom.current().nextInt())
+                .filter(i -> i > 0 && i != 1)
+                .limit(5)
+                .forEach(i -> assertThat(dominanceOfCaseLabels2(i)).isEqualTo("Positive"));
+
+        IntStream.generate(() -> ThreadLocalRandom.current().nextInt())
+                .filter(i -> i <= 0 && i != -1)
+                .limit(5)
+                .forEach(i -> assertThat(dominanceOfCaseLabels2(i)).isEqualTo("Not Positive"));
+    }
+
+    sealed interface S permits A, B, C {
+    }
+
+    static final class A implements S {
+    }
+
+    static final class B implements S {
+    }
+
+    record C(int i) implements S {
+    }
+
+    private String sealedClass1(S s) {
+        return switch (s) {
+            case A a -> "A";
+            case B b -> "B";
+            case C(int i) -> String.valueOf(i);
+        };
+    }
+
+    sealed interface I1<T> permits A1, B1 {
+    }
+
+    static final class A1<X> implements I1<String> {
+    }
+
+    static final class B1<Y> implements I1<Y> {
+    }
+
+    private String sealedClass2(I1<Integer> i1) {
+        /*
+         * i1 限定了泛型是 Integer
+         * A1 虽然实现了 I1，但是其 I1 的泛型是 String，永远不会匹配 I1<Integer>
+         */
+        return switch (i1) {
+            case B1<Integer> bi -> "212";
+        };
+    }
+    
+    @Test
+    public void testExhaustivenessAndSealedClass() {
+        assertThat(sealedClass1(new A())).isEqualTo("A");
+        assertThat(sealedClass1(new B())).isEqualTo("B");
+        assertThat(sealedClass1(new C(100))).isEqualTo("100");
+
+        assertThat(sealedClass2(new B1<>())).isEqualTo("212");
     }
 }
